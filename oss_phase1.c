012a2dfebfe3311d313f7e43ab7fb0c9f287b3da
sihyun <sihyunnn33@gmail.com>
2024-06-05 22:46:43 +0900

    Add base variables

---
+++ oss_phase1.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+	int com[3] = { 0, 0, 0 };
+	int user[3] = { 0, 0, 0 };
+	int challenge = 1;
+	int strike = 0, ball = 0;
+
+	return 0;
+}
